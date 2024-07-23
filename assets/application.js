2237291a7b1790162f00ef951ecc428d9cef6bb3
vyuba <preyealexander731@gmail.com>
2024-07-23 21:51:01 +0100

    told me to change the color again

--- assets/application.js
+++ assets/application.js
@@ -400,3 +400,32 @@ function imageLoaded(img) {
     loader.style.display = 'none';
   }
 }
+
+/**for recomendation section */
+const handleIntersection = (entries, observer) => {
+  if (!entries[0].isIntersecting) return;
+
+  observer.unobserve(productRecommendationsSection);
+
+  const url = productRecommendationsSection.dataset.url;
+
+  fetch(url)
+    .then(response => response.text())
+    .then(text => {
+      const html = document.createElement('div');
+      html.innerHTML = text;
+      const recommendations = html.querySelector('.product-recommendations');
+
+      if (recommendations && recommendations.innerHTML.trim().length) {
+        productRecommendationsSection.innerHTML = recommendations.innerHTML;
+      }
+    })
+    .catch(e => {
+      console.error(e);
+    });
+};
+
+const productRecommendationsSection = document.querySelector('.product-recommendations');
+const observer = new IntersectionObserver(handleIntersection, {rootMargin: '0px 0px 200px 0px'});
+
+observer.observe(productRecommendationsSection);
